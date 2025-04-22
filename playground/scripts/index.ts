47a729fb3ecf5fab0169b63701c50c239577b6cc
kiv1n <aloha@kiv1n.ru>
2025-04-22 15:10:30 +0300

    Refactor cheqd functions

--- playground/scripts/index.ts
+++ playground/scripts/index.ts
@@ -1,32 +1,44 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
 import * as dotenv from "dotenv";
+import { cheqdConfig } from "./config/cheqd";
+import {
+  createCheqdDid,
+  createCheqdResource,
+  listCheqdDids,
+} from "./lib/cheqd";
 dotenv.config();
+
+async function listDids() {
+  const { data } = await listCheqdDids();
+  console.log("Data:", data);
+}
+
+async function createDid() {
+  const params = {
+    network: "testnet",
+    identifierFormatType: "uuid",
+    verificationMethodType: "Ed25519VerificationKey2018",
+  };
+  const { data } = await createCheqdDid(params);
+  console.log("Data:", data);
+}
+
+async function createResource() {
+  const params = {
+    data: Buffer.from(
+      JSON.stringify(cheqdConfig.aiAgentAuthorisationSchema)
+    ).toString("base64"),
+    encoding: "base64",
+    name: "AIAgentAuthorisation",
+    type: "JSONSchemaValidator2020",
+  };
+  const { data } = await createCheqdResource(params);
+  console.log("Data:", data);
+}
 
 async function main() {
   console.log("Starting script...");
-
-  // List DIDs
-  // const { data } = await listDids();
-
-  // Create a new DID
-  // const params = {
-  //   network: "testnet",
-  //   identifierFormatType: "uuid",
-  //   verificationMethodType: "Ed25519VerificationKey2018",
-  // };
-  // const { data } = await createDid(params);
-
-  // Create a new DID linked resource
-  // const params = {
-  //   data: Buffer.from(
-  //     JSON.stringify(cheqdConfig.aiAgentAuthorisationSchema)
-  //   ).toString("base64"),
-  //   encoding: "base64",
-  //   name: "AIAgentAuthorisation",
-  //   type: "JSONSchemaValidator2020",
-  // };
-  // const { data } = await createDidLinkedResource(params);
-
-  // console.log("Data:", data);
 }
 
 main().catch((error) => {
--- playground/scripts/lib/cheqd.ts
+++ playground/scripts/lib/cheqd.ts
@@ -1,19 +1,19 @@
 import axios from "axios";
 import { cheqdConfig } from "../config/cheqd";
 
-export async function listDids() {
+export async function listCheqdDids() {
   return axios.get(`${cheqdConfig.apiHost}/did/list`, {
     headers: { "x-api-key": `${cheqdConfig.apiKey}` },
   });
 }
 
-export async function createDid(params: object) {
+export async function createCheqdDid(params: object) {
   return await axios.post(`${cheqdConfig.apiHost}/did/create`, params, {
     headers: { "x-api-key": `${cheqdConfig.apiKey}` },
   });
 }
 
-export async function createDidLinkedResource(params: object) {
+export async function createCheqdResource(params: object) {
   return await axios.post(
     `${cheqdConfig.apiHost}/resource/create/${cheqdConfig.rootDid}`,
     params,
